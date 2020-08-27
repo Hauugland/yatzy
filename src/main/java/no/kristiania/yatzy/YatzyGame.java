7190d5d2a8fb3f83860ad6688b49fa4973d38374
Hauugland <46930504+Hauugland@users.noreply.github.com>
2020-08-27 11:16:07 +0200

    Implement the rest of the normal yatzy rules (#2)
    * Complete TWO_PAIRS(green), Start on FOUR_OF_A_KIND(red)
    
    * Complete FOUR_OF_A_KIND(green), start on YATZY(red)
    
    * Complete YATZY(green), Start on SMALL_STRAIGHT
    
    * Complete SMALL_STRAIGHT(green), start LARGE_STRAIGHT(red)
    
    * Complete LARGE_STRAIGHT(green), start FULL_HOUSE(red)
    
    * Complete CHANCE(green) and FULL_HOUSE(green), start THREES(red)
    
    * Change SMALL and LARGE_STRAIGHT

--- src/main/java/no/kristiania/yatzy/YatzyGame.java
+++ src/main/java/no/kristiania/yatzy/YatzyGame.java
@@ -10,6 +10,30 @@ public class YatzyGame {
             frequencies[die]++;
         }
 
+        if(category == YatzyCategory.ONES){
+            return frequencies[1];
+        }
+
+        if(category == YatzyCategory.TWOS ){
+            return frequencies[2] * 2;
+        }
+
+        if(category == YatzyCategory.THREES){
+            return frequencies[3] * 3;
+        }
+
+        if(category == YatzyCategory.FOURS){
+            return  frequencies[4] * 4;
+        }
+
+        if(category == YatzyCategory.FIVES){
+            return  frequencies[5] * 5;
+        }
+
+        if(category == YatzyCategory.SIXES){
+            return  frequencies[6] * 6;
+        }
+
         if(category == YatzyCategory.PAIR){ //Trenger hjelp
 
             for(int value = frequencies.length - 1; value >= 0; value--){
@@ -18,6 +42,15 @@ public class YatzyGame {
                 }
             }
 
+        }
+
+        if(category == YatzyCategory.TWO_PAIRS){ //Egenlaget
+
+            for(int value = frequencies.length - 1; value >= 0; value--){
+                if(frequencies[value] == 2){
+                    result += value * 2;
+                }
+            }
         }
 
         if(category == YatzyCategory.THREE_OF_A_KIND){ //Trenger hjelp
@@ -30,13 +63,59 @@ public class YatzyGame {
 
         }
 
-        if(category == YatzyCategory.ONES){
-            return frequencies[1];
+        if(category == YatzyCategory.FOUR_OF_A_KIND){
+
+            for(int value = frequencies.length - 1; value >= 0; value--){
+                if(frequencies[value] == 4){
+                    return value * 4;
+                }
+            }
+
         }
 
-        if(category == YatzyCategory.TWOS ){
-            return frequencies[2] * 2;
+        if(category == YatzyCategory.YATZY){
+
+            for(int value = frequencies.length - 1; value >= 0; value--){
+                if(frequencies[value] == 5){
+                    return 50;
+                }
+            }
         }
+
+        if(category == YatzyCategory.SMALL_STRAIGHT){
+
+            if(frequencies[1] >= 1 && frequencies[2] >= 1 && frequencies[3] >= 1 && frequencies[4] >= 1 && frequencies[5] >= 1){
+                return 15;
+            }
+        }
+
+        if(category == YatzyCategory.LARGE_STRAIGHT){
+            if(frequencies[2] >= 1 && frequencies[3] >= 1 && frequencies[4] >= 1 && frequencies[5] >= 1 && frequencies[6] >= 1){
+                return 20;
+            }
+        }
+
+        if(category == YatzyCategory.FULL_HOUSE){
+
+            for(int value = frequencies.length - 1; value >= 0; value--){
+                if(frequencies[value] == 3){
+                    result += value * 3;
+                }
+            }
+            for(int value = frequencies.length - 1; value >= 0; value--){
+                if(frequencies[value] == 2){
+                    result += value * 2;
+                }
+            }
+        }
+
+        if(category == YatzyCategory.CHANCE){
+            for (int die : dice) {
+                result += die;
+            }
+        }
+
+
 
         return result;
     }
--- src/test/java/no/kristiania/yatzy/YatzyTest.java
+++ src/test/java/no/kristiania/yatzy/YatzyTest.java
@@ -21,6 +21,30 @@ public class YatzyTest {
     }
 
     @Test
+    void shouldScoreThrees(){
+        assertEquals(6, yatzyGame.score(YatzyCategory.THREES, new int[] {3, 4, 3, 6, 1}));
+        assertEquals(9, yatzyGame.score(YatzyCategory.THREES, new int[] {6, 3, 1, 3, 3}));
+    }
+
+    @Test
+    void shouldScoreFours(){
+        assertEquals(8, yatzyGame.score(YatzyCategory.FOURS, new int[] {3, 4, 3, 4, 1}));
+        assertEquals(16, yatzyGame.score(YatzyCategory.FOURS, new int[] {4, 4, 1, 4, 4}));
+    }
+
+    @Test
+    void shouldScoreFives(){
+        assertEquals(10, yatzyGame.score(YatzyCategory.FIVES, new int[] {3, 5, 5, 4, 1}));
+        assertEquals(5, yatzyGame.score(YatzyCategory.FIVES, new int[] {4, 5, 1, 4, 4}));
+    }
+
+    @Test
+    void shouldScoreSixes(){
+        assertEquals(18, yatzyGame.score(YatzyCategory.SIXES, new int[] {3, 6, 6, 6, 1}));
+        assertEquals(6, yatzyGame.score(YatzyCategory.SIXES, new int[] {4, 6, 1, 4, 4}));
+    }
+
+    @Test
     void shouldScorePair(){
         assertEquals(10, yatzyGame.score(YatzyCategory.PAIR, new int[] {1, 3, 5, 5, 2}));
         assertEquals(12, yatzyGame.score(YatzyCategory.PAIR, new int[] {1, 3, 6, 6, 2}));
@@ -28,10 +52,57 @@ public class YatzyTest {
     }
 
     @Test
+    void shouldScoreTwoPairs(){
+        assertEquals(14, yatzyGame.score(YatzyCategory.TWO_PAIRS, new int[] {1, 1, 6, 6, 2}));
+        assertEquals(22, yatzyGame.score(YatzyCategory.TWO_PAIRS, new int[] {5, 5, 6, 6, 2}));
+    }
+
+    @Test
     void shouldScoreThreeOfAKind(){
         assertEquals(12, yatzyGame.score(YatzyCategory.THREE_OF_A_KIND, new int[] {4, 4, 6, 4, 2}));
         assertEquals(18, yatzyGame.score(YatzyCategory.THREE_OF_A_KIND, new int[] {4, 6, 6, 4, 6}));
     }
+
+    @Test
+    void shouldScoreFourOfAKind(){
+        assertEquals(12, yatzyGame.score(YatzyCategory.FOUR_OF_A_KIND, new int[] {3, 3, 6, 3, 3}));
+        assertEquals(20, yatzyGame.score(YatzyCategory.FOUR_OF_A_KIND, new int[] {5, 5, 6, 5, 5}));
+    }
+
+    @Test
+    void shouldScoreYatzy(){
+        assertEquals(50, yatzyGame.score(YatzyCategory.YATZY, new int[] {6, 6, 6, 6, 6}));
+        assertEquals(50, yatzyGame.score(YatzyCategory.YATZY, new int[] {1, 1, 1, 1, 1}));
+    }
+
+    @Test
+    void shouldScoreSmallStraight(){
+        assertEquals(15, yatzyGame.score(YatzyCategory.SMALL_STRAIGHT, new int[] {3, 5, 1, 4, 2}));
+        assertEquals(15, yatzyGame.score(YatzyCategory.SMALL_STRAIGHT, new int[] {4, 3, 2, 5, 1}));
+    }
+
+    @Test
+    void shouldScoreLargeStraight(){
+        assertEquals(20, yatzyGame.score(YatzyCategory.LARGE_STRAIGHT, new int[] {3, 5, 6, 4, 2}));
+        assertEquals(20, yatzyGame.score(YatzyCategory.LARGE_STRAIGHT, new int[] {4, 3, 2, 5, 6}));
+    }
+
+    @Test
+    void shouldScoreFullHouse(){
+        assertEquals(19, yatzyGame.score(YatzyCategory.FULL_HOUSE, new int[] {3, 5, 3, 3, 5}));
+        assertEquals(18, yatzyGame.score(YatzyCategory.FULL_HOUSE, new int[] {6, 2, 2, 2, 6}));
+    }
+
+    @Test
+    void shouldScoreChance(){
+        assertEquals(20, yatzyGame.score(YatzyCategory.CHANCE, new int[] {4, 5, 3, 3, 5}));
+        assertEquals(28, yatzyGame.score(YatzyCategory.CHANCE, new int[] {6, 4, 6, 6, 6}));
+    }
+
+
+
+
+
 
 
 
